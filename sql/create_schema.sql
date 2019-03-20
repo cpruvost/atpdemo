37c05b75382e881a4d45292eaa8cabc42a2f0be2
quizforparty44 <quizforparty44@gmail.com>
2019-03-20 16:49:22 +0100

    Stage Schema in Atp

--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -1,24 +1,7 @@
-set serveroutput on;
+set echo off head off feed off pagesize 0 trimspool on linesize 1000 colsep ,
+spool output.csv
+SELECT Count(*) FROM all_users WHERE  USERNAME = 'MEDREC';
+spool off;
 
-DECLARE
-    cnt NUMBER;
-BEGIN
-    SELECT Count(*)
-    INTO   cnt
-    FROM   all_users
-    WHERE  USERNAME = 'MEDREC';
-
-    IF cnt = 1 THEN
-	
-        dbms_output.put_line('User MEDREC Already Exists');   
-    
-    ELSE
-		EXECUTE IMMEDIATE 'CREATE USER medrec IDENTIFIED BY ATPwelcome1234';
-		EXECUTE IMMEDIATE 'GRANT DWROLE to medrec';
-		
-		dbms_output.put_line('User MEDREC Created');   
-    END IF;
-END;
-/
 
 
