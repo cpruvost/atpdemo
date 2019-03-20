c49bc112291c5a25982ea959600f7484e22810f4
quizforparty44 <quizforparty44@gmail.com>
2019-03-20 17:08:03 +0100

    Stage Schema in Atp

--- sql/check_schema.sql
+++ sql/check_schema.sql
@@ -1,4 +1,4 @@
-set echo off head off feed off pagesize 0 trimspool on linesize 1000 colsep ,
+set echo off head off feed off pagesize 0 trimspool on linesize 1000 colsep
 spool output.csv
 SELECT Count(*) FROM all_users WHERE  USERNAME = 'MEDREC';
 spool off;
